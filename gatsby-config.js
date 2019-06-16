8f1eec43edc131e07c48e95c070da08f5ce02b2d
engmagdy87 <eng.magdy87@gmail.com>
2019-06-16 20:42:48 +0200

    change manifest theme and background color

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,8 +62,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#0066b3`,
+        theme_color: `#0066b3`,
         display: `minimal-ui`,
         icon: `static/uploads/assets/manifest-logo.png`, // This path is relative to the root of the site.
       },
