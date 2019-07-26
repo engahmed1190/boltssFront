b1f11ae480fbee04cd91aa3aa11db882cb8d5265
engmagdy87 <eng.magdy87@gmail.com>
2019-07-26 15:34:55 +0200

    uninstall gatsby-plugin-manifest plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,18 +56,18 @@ module.exports = {
 
     `gatsby-plugin-netlify-cms`,
     // netlifyCmsPaths,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#0066b3`,
-        theme_color: `#0066b3`,
-        display: `minimal-ui`,
-        icon: `static/uploads/assets/manifest-logo.png`, // This path is relative to the root of the site.
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `gatsby-starter-default`,
+    //     short_name: `starter`,
+    //     start_url: `/`,
+    //     background_color: `#0066b3`,
+    //     theme_color: `#0066b3`,
+    //     display: `minimal-ui`,
+    //     icon: `static/uploads/assets/manifest-logo.png`, // This path is relative to the root of the site.
+    //   },
+    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
